4e03a7f5383d308241b7e1acf43ca165dd78cebd
mkdir <3044788388@qq.com>
2025-08-28 11:50:31 +0800

    小调整

--- src/main/java/org/genntii/mkdir/controller/PersonalController.java
+++ src/main/java/org/genntii/mkdir/controller/PersonalController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /**
- *
- *
+ * 个人管理控制器
+ * 处理与用户个人页面相关的请求
  *
  * @author mkdir
  * @since 2025/08/27 11:00
@@ -16,11 +16,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class PersonalController {
 
+    /**
+     * 处理访问管理者页面的GET请求
+     * 根据用户ID跳转到对应的管理者页面
+     *
+     * @param id 用户唯一标识符，通过URL路径传入
+     * @return 返回manager页面视图名称
+     */
     @GetMapping("/manager/{id}")
     public String message(@PathVariable("id") Long id) {
         log.info("访问manager页面 对象用户ID：{}",id);
         return "manager"; // 对应 manager.html
     }
-
 
 }
